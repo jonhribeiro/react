3b5b612beff6526f0075c19089a0058d2260d2cb
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-24 17:51:09 -0300

    refactor: movendo o SUT e criando o novo metodo

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,11 +2,24 @@ import { HttpPostClient } from "data/protocols/http/http-post-client"
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "../../test/mock-http-client"
 
-describe('RemoteAuthentication', () => {
-  test('deve chamar HttpPostClient com o URL correto', async () => {
-    const url = 'any_url'
+type SutTypes = {
+  sut: RemoteAuthentication
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url = 'any_url'): SutTypes => {
     const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    return {
+        sut,
+        httpPostClientSpy
+    }
+}
+
+describe('RemoteAuthentication', () => {
+  test('deve chamar HttpPostClient com o URL correto', async () => {
+    const url = 'other_url'
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
   })
