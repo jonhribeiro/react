bf7d3a33a1dcf5cc24d841b6fb95f04e578378b8
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-24 17:04:08 -0300

    feat: inserindo o RemoteAuthentication com HttpPostClient com a url correta

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient } from "data/protocols/http/http-post-client";
+
+export class RemoteAuthentication {
+    constructor (
+      private readonly url: string,
+      private readonly httppostClient: HttpPostClient
+    ) {}
+  
+    async auth (): Promise<void> {
+      await this.httppostClient.post(this.url)
+    }
+  }
