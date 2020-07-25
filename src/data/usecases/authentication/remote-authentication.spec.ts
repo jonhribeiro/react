d48b8f937e9a3e837cf00a855793afb4c9f3503c
jonh <jonh.araujo@aparecida.go.gov.br>
2020-07-24 23:14:20 -0300

    refactor: funcao RemoteAuthentication usando a biblioteca faker

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,13 +1,13 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client"
 import { RemoteAuthentication } from "./remote-authentication"
 import { HttpPostClientSpy } from "../../test/mock-http-client"
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url = 'any_url'): SutTypes => {
+const makeSut = (url = faker.internet.url()): SutTypes => {
     const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(url, httpPostClientSpy)
     return {
@@ -18,7 +18,7 @@ const makeSut = (url = 'any_url'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   test('deve chamar HttpPostClient com o URL correto', async () => {
-    const url = 'other_url'
+    const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
     expect(httpPostClientSpy.url).toBe(url)
