353c42a438911421c582582e75fc777c74eb8c64
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-27 17:36:19 -0300

    refactor: mover a criação da solicitação de postagem para um método de fábrica

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import { AxiosHttpClient } from './axios-http-cliente'
 import axios from 'axios'
 import faker from 'faker'
+import { HttpPostParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
@@ -9,11 +10,18 @@ const makeSut = (): AxiosHttpClient => {
     return new AxiosHttpClient()
 }
 
+const mockPostRequest = (): HttpPostParams<any> => ({
+    url: faker.internet.url(),
+    body: faker.random.objectElement()
+})
+
 describe('AxiosHttpClient', () => {
     test('deve chamar axios com o URL correto com verbo correto', async () => {
-        const url = faker.internet.url()
+        const request = mockPostRequest()
         const sut = makeSut()
-        await sut.post({ url })
-        expect(mockedAxios.post).toHaveBeenCalledWith(url)
+        await sut.post(request)
+        expect(mockedAxios.post).toHaveBeenCalledWith(request.url)
     }) 
+
+    
 })
