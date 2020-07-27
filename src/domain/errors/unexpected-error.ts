6d91c9f07e988485e0c3f09e44512d4631b7116a
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-27 14:38:25 -0300

    feat: testando RemothAuthentication esperando os erro default nos casos

---
+++ src/domain/errors/unexpected-error.ts
@@ -0,0 +1,6 @@
+export class UnexpectedError extends Error {
+    constructor () {
+        super('Algo errado aconteceu. tente novamente mais tarde')
+        this.name = 'UnexpectedError'
+    }
+}
