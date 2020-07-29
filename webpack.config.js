88f36ca611bd5c5b390cd0c856c3a7e6e7e77128
jonh <jonh.araujo@aparecida.go.gov.br>
2020-07-29 00:01:37 -0300

    chore: configurando o sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
@@ -10,10 +12,29 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', '.scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
+    },
+    module: {
+        rules: [{
+            test: /\.ts(x?)$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.s(css?)$/,
+            use: [{
+                loader: 'style-loader'
+            }, {
+                loader: 'css-loader',
+                options: {
+                    module: true
+                }
+            }, {
+                loader: 'sass-loader'
+            }]
+        }]
     },
     devServer: {
         contentBase: './public',
