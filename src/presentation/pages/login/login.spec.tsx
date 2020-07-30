c7fb55b615269f2fa03cb544bf4fc503c5c4ee7d
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-30 11:06:26 -0300

    test: verifique se o login não carregará o spinner e o erro ao iniciar

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('login componente', () => {
-    test('', () => {
-        render(<Login />)
+    test('devemos não renderiza gerando erro no início', () => {
+        const { getByTestId } = render(<Login />)
+        const errorWrap = getByTestId('error-wrap')
+        expect(errorWrap.childElementCount).toBe(0)
     })
 })
