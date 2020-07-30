36b3572c42a1e42f9c06f8297080792b5809eee8
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-30 11:24:50 -0300

    test: verifique se o login começa com o botão enviar desativado

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react'
 import Login from './login'
 
 describe('login componente', () => {
-    test('devemos não renderiza gerando erro no início', () => {
+    test('devemos não renderiza gerando erro no início e com o button desabilitado', () => {
         const { getByTestId } = render(<Login />)
         const errorWrap = getByTestId('error-wrap')
         expect(errorWrap.childElementCount).toBe(0)
+        const submitButton = getByTestId('submit') as HTMLButtonElement
+        expect(submitButton.disabled).toBe(true)
     })
 })
