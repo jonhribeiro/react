98287aabbc2389e50d97190acbbf3fb40bc66ae6
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-07-30 11:24:24 -0300

    feat: verifique se o login começa com o botão enviar desativado

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -21,7 +21,7 @@ const Login: React.FC = () => {
                     <h2>Login</h2>
                     <Input type="email" name="email" placeholder="Digite o E-mail"/>
                     <Input type="password" name="password" placeholder="Digite a Senha"/>                
-                    <button className={Styles.submit} type="submit">Entrar</button>
+                    <button data-testid="submit" disabled className={Styles.submit} type="submit">Entrar</button>
                     <span className={Styles.link}>Criar Conta</span>
                     <FormStatus />
                 </form>
