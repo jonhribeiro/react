ebf824ef8e264d225dc6a90a1d7c40b1b5d5092e
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-03 16:03:17 -0300

    refactor: movendo pasta para dentro do validators

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,19 @@
 import { EmailValidation } from "./email-validation"
 import { InvalidFieldError } from "@/validation/errors"
+import faker from 'faker'
+
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column()) 
 
 describe('EmailValidation', () => {
     test('deve retornar erro se o email for inválido', () => {
-        const sut = new EmailValidation('email')
-        const error = sut.validate('')
+        const sut = makeSut()
+        const error = sut.validate(faker.random.word())
         expect(error).toEqual(new InvalidFieldError())
+    })
+    
+    test('deve retornar falso se o email for válido', () => {
+        const sut = makeSut()
+        const error = sut.validate(faker.internet.email())
+        expect(error).toBeFalsy()
     })
 })
