3913e8deb2c0fff6fa9b5cf37293578893f83993
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-03 11:20:49 -0300

    refactor: mover a criação sut para um método de fábrica"

--- src/validation/required-field/require-field-validations.spec.ts
+++ src/validation/required-field/require-field-validations.spec.ts
@@ -2,15 +2,17 @@ import { RequiredFieldValidation } from "./required-field-validation"
 import { RequiredFieldError } from "@/validation/errors"
 import faker from 'faker'
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column()) 
+
 describe('RequiredFieldValidation', () => {
     test('deve retornar erro se o campo estiver vazio', () => {
-        const sut = new RequiredFieldValidation('email')
+        const sut = makeSut()
         const error = sut.validate('')
         expect(error).toEqual(new RequiredFieldError())
     })
 
     test('deve retornar falso se o campo não estiver vazio', () => {
-        const sut = new RequiredFieldValidation('email')
+        const sut = makeSut()
         const error = sut.validate(faker.random.word())
         expect(error).toBeFalsy()
     })
