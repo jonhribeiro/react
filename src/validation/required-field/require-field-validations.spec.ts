266596e4003171cf96704e34739c37874f3d6617
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-03 11:13:30 -0300

    test: verifique se RequiredFieldValidation retorna falso se o campo não estiver vazio

--- src/validation/required-field/require-field-validations.spec.ts
+++ src/validation/required-field/require-field-validations.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldValidation } from "./required-field-validation"
 import { RequiredFieldError } from "@/validation/errors"
+import faker from 'faker'
 
 describe('RequiredFieldValidation', () => {
     test('deve retornar erro se o campo estiver vazio', () => {
         const sut = new RequiredFieldValidation('email')
         const error = sut.validate('')
         expect(error).toEqual(new RequiredFieldError())
+    })
+
+    test('deve retornar falso se o campo não estiver vazio', () => {
+        const sut = new RequiredFieldValidation('email')
+        const error = sut.validate(faker.random.word())
+        expect(error).toBeFalsy()
     })
 })
