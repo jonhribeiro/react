bf41e623bd8556edcaad4c570c1f1b93f11a1985
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-03 16:50:48 -0300

    feat: validando campo com quantidade minima

--- src/validation/validators/min-length/min-lenght-validation.spec.ts
+++ src/validation/validators/min-length/min-lenght-validation.spec.ts
@@ -1,10 +1,19 @@
 import { InvalidFieldError } from "@/validation/errors"
 import { MinLengthValidation } from "./min-length-validation"
+import faker from 'faker'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5) 
 
 describe('MinLengthValidation', () => {
     test('deve retornar erro se o valor for inválido', () => {
-        const sut = new MinLengthValidation('field', 5)
-        const error = sut.validate('123')
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(4))
         expect(error).toEqual(new InvalidFieldError())
+    })
+
+    test('deve retornar falso se o valor for válido', () => {
+        const sut = makeSut()
+        const error = sut.validate(faker.random.alphaNumeric(5))
+        expect(error).toBeFalsy()
     })
 })
