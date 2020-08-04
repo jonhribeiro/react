310999ffaa175554c5506717c38cccb6748bf4ce
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-04 13:33:27 -0300

    feat: garantir que ValidationBuiulder tenha um auxiliar de email

--- src/validation/validators/builder/validate-buiulder.spec.ts
+++ src/validation/validators/builder/validate-buiulder.spec.ts
@@ -1,4 +1,4 @@
-import { RequiredFieldValidation } from "@/validation/validators"
+import { RequiredFieldValidation, EmailValidation } from "@/validation/validators"
 import { ValidationBuilder as sut } from "./validation-buiulder"
 
 
@@ -7,5 +7,10 @@ describe('ValidationBuilder', () => {
     test('deve retornar RequiredFieldValidation', () => {
         const validations = sut.field('any_field').required().build()
         expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+    })
+
+    test('deve retornar EmailValidation', () => {
+        const validations = sut.field('any_field').email().build()
+        expect(validations).toEqual([new EmailValidation('any_field')])
     })
 })
