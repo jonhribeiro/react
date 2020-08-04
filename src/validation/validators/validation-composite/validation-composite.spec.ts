bca92f778a1adbb767f1b2b664f6a4b3f394b467
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-04 09:56:25 -0300

    test: testando o validationcompositer se falar 1 falha todos

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,13 +5,14 @@ import { ValidationComposite } from "./validate-composite"
 describe('ValidationComposite', () => {
     test('deve retornar erro se alguma validação falhar', () => {
         const fieldValidationspy = new FieldValidationSpy('any_field')
+        fieldValidationspy.error = new Error('first_error_message')
         const fieldValidationspy2 = new FieldValidationSpy('any_field')
-        fieldValidationspy2.error = new Error('any_erro_message')
+        fieldValidationspy2.error = new Error('second_error_message')
         const sut = new ValidationComposite([
             fieldValidationspy,
             fieldValidationspy2
         ])
         const error = sut.validate('any_field', 'any_value')
-        expect(error).toBe('any_erro_message')
+        expect(error).toBe('first_error_message')
     })
 })
