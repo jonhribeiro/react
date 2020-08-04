a57d5d9df3b918236d5f00307a1ea1f382be7155
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-04 14:17:13 -0300

    test: deve retornar uma lista de validações

--- src/validation/validators/builder/validate-buiulder.spec.ts
+++ src/validation/validators/builder/validate-buiulder.spec.ts
@@ -1,21 +1,35 @@
 import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from "@/validation/validators"
 import { ValidationBuilder as sut } from "./validation-buiulder"
-
-
+import faker from 'faker'
 
 describe('ValidationBuilder', () => {
     test('deve retornar RequiredFieldValidation', () => {
-        const validations = sut.field('any_field').required().build()
-        expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+        const field = faker.database.column()
+        const validations = sut.field(field).required().build()
+        expect(validations).toEqual([new RequiredFieldValidation(field)])
     })
 
     test('deve retornar EmailValidation', () => {
-        const validations = sut.field('any_field').email().build()
-        expect(validations).toEqual([new EmailValidation('any_field')])
+        const field = faker.database.column()
+        const validations = sut.field(field).email().build()
+        expect(validations).toEqual([new EmailValidation(field)])
     })
 
     test('deve retornar MinLengthValidation', () => {
-        const validations = sut.field('any_field').min(5).build()
-        expect(validations).toEqual([new MinLengthValidation('any_field', 5)])
+        const field = faker.database.column()
+        const length = faker.random.number()
+        const validations = sut.field(field).min(length).build()
+        expect(validations).toEqual([new MinLengthValidation(field, length)])
+    })
+
+    test('deve retornar uma lista de validações', () => {
+        const field = faker.database.column()
+        const length = faker.random.number()
+        const validations = sut.field(field).required().min(length).email().build()
+        expect(validations).toEqual([
+            new RequiredFieldValidation(field),
+            new MinLengthValidation(field, length),
+            new EmailValidation(field)
+        ])
     })
 })
