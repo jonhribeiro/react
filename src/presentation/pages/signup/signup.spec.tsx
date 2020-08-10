14fee874d8e9ad1c237dcde2d5ff90123d0ec0a9
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-10 16:09:58 -0300

    feat: certifique-se de que a inscrição altere o status do e-mail para erro se a validação falhar

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -35,7 +35,7 @@ describe('SignUp componente', () => {
         Helper.testChildCount(sut, 'error-wrap', 0)
         Helper.testButtonIsDisabled(sut, 'submit', true)
         Helper.testStatusForField(sut, 'name', validationError)
-        Helper.testStatusForField(sut, 'email', 'Campo obrigatório')
+        Helper.testStatusForField(sut, 'email', validationError)
         Helper.testStatusForField(sut, 'password', 'Campo obrigatório')
         Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
     })
@@ -45,5 +45,12 @@ describe('SignUp componente', () => {
         const { sut } = makeSut({validationError})
         Helper.populateField(sut, 'name')
         Helper.testStatusForField(sut, 'name', validationError)
+    })
+
+    test('deve chamar o email e testar a falha na validacao', () => {
+        const validationError = faker.random.words()
+        const { sut } = makeSut({validationError})
+        Helper.populateField(sut, 'email')
+        Helper.testStatusForField(sut, 'email', validationError)
     })
 })
