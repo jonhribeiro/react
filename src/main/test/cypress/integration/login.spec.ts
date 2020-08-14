70de85b3ad038db0fd9ca5ec1542c02704507dac
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-14 14:46:20 -0300

    test: deve apresentar erro se o formulário for inválido

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,13 +1,31 @@
+import faker from 'faker'
+
 describe('Login', () => {
     beforeEach(() => {
         cy.visit('login')
     })
+    
     it('deve carregar com o estado inicial correto', () => {
+        cy.getByTestId('email').should('have.attr', 'readonly')
         cy.getByTestId('email-status')
             .should('have.attr', 'title', 'Campo obrigatório')
             .should('contain.text', '🔴')
+        cy.getByTestId('password').should('have.attr', 'readonly')
         cy.getByTestId('password-status')
             .should('have.attr', 'title', 'Campo obrigatório')
+            .should('contain.text', '🔴')
+        cy.getByTestId('submit').should('have.attr', 'disabled')
+        cy.getByTestId('error-wrap').should('not.have.descendants')
+    })
+
+    it('deve apresentar erro se o formulário for inválido', () => {
+        cy.getByTestId('email').focus().type(faker.random.word())
+        cy.getByTestId('email-status')
+            .should('have.attr', 'title', 'Valor Invalido')
+            .should('contain.text', '🔴')
+        cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+        cy.getByTestId('password-status')
+            .should('have.attr', 'title', 'Valor Invalido')
             .should('contain.text', '🔴')
         cy.getByTestId('submit').should('have.attr', 'disabled')
         cy.getByTestId('error-wrap').should('not.have.descendants')
