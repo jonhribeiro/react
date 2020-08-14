0ebfb0f8a4d779f957e619007005411224ab20a8
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-14 08:36:55 -0300

    feat: adicionando o Composer do signup

---
+++ src/main/factories/usecases/add-account/remote-add-account-factory.ts
@@ -0,0 +1,10 @@
+
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-client-factory'
+import { AddAccount } from '@/domain/usercases'
+import { RemoteAddAccount } from '@/data/usecases/add-account/remote-add-account'
+
+export const makeRemoteAddAccount = (): AddAccount => {
+    return new RemoteAddAccount(makeApiUrl('/signup'), makeAxiosHttpClient())
+
+}
