4575f45414f3394084c997f3a0ddd90f44c36eb1
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-14 13:47:51 -0300

    chore: adicionando comando para o cypress

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,8 @@
 describe('Login', () => {
-    it('deve carregar com o estado inicial correto', () => {
+    beforeEach(() => {
         cy.visit('login')
+    })
+    it('deve carregar com o estado inicial correto', () => {
+        cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
     })
 })
