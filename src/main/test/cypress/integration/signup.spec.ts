a11c871692e93fa5cfef7424fc74c8fd05b38d1c
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-18 11:02:46 -0300

    test: deve apresentar erro se o formulário for inválido

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,4 +1,5 @@
 import * as FormHelper from '../support/form-helper'
+import faker from 'faker'
 
 describe('SignUp', () => {
     beforeEach(() => {
@@ -16,5 +17,19 @@ describe('SignUp', () => {
         FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
         cy.getByTestId('submit').should('have.attr', 'disabled')
         cy.getByTestId('error-wrap').should('not.have.descendants') 
+    })
+
+    
+    it('deve apresentar erro se o formulário for inválido', () => {
+        cy.getByTestId('name').focus().type(faker.random.alphaNumeric(3))
+        FormHelper.testInputStatus('name', 'Valor Invalido')
+        cy.getByTestId('email').focus().type(faker.random.word())
+        FormHelper.testInputStatus('email', 'Valor Invalido')
+        cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+        FormHelper.testInputStatus('password', 'Valor Invalido')
+        cy.getByTestId('passwordConfirmation').focus().type(faker.random.alphaNumeric(4))
+        FormHelper.testInputStatus('passwordConfirmation', 'Valor Invalido')
+        cy.getByTestId('submit').should('have.attr', 'disabled')
+        cy.getByTestId('error-wrap').should('not.have.descendants')
     })
 })
