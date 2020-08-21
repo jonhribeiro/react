efeb1fab9d92ccbfc2f0b188b1ff2f49e937cd97
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-21 11:31:33 -0300

    test: adapter o get

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -3,16 +3,28 @@ import 'jest-localstorage-mock'
 import { LocalStorageAdapter } from './local-storage-adapter'
 import { AccountModel } from '@/domain/models'
 
+const makeSut = (): LocalStorageAdapter => new LocalStorageAdapter()
+
 describe('LocalStorageAdapter', () => {
     beforeEach(() => {
       localStorage.clear()
     })
   
     test('Deve chamar localStorage.setItem com valores corretos', async () => {
-      const sut = new LocalStorageAdapter()
+      const sut = makeSut()
       const key = faker.database.column()
       const value = faker.random.objectElement<AccountModel>()
       sut.set(key, value)
       expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+    })
+  
+    test('Deve chamar localStorage.getItem com valor correto', async () => {
+      const sut = makeSut()
+      const key = faker.database.column()
+      const value = faker.random.objectElement<AccountModel>()
+      const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+      const obj = sut.get(key)
+      expect(obj).toEqual(value)
+      expect(getItemSpy).toHaveBeenCalledWith(key)
     })
 })
