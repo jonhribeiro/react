2a48599162eebc9bf755de57c5e396446f98c227
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-21 18:07:12 -0300

    test: testando url

--- src/presentation/components/private-route/private-route.spec.tsx
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import PrivateRoute from './private-route'
+import { ApiContext } from '@/presentation/contexts'
+import { mockAcountModel } from '@/domain/test'
 
-describe('PrivateRoute', () => {
-    test('Rediricionar para logim se nao tiver o token de acesso', () => {
-        const history = createMemoryHistory({ initialEntries: ['/'] })
-        render(
+type SutTypes = {
+    history: MemoryHistory
+}
+
+const makeSut = (account = mockAcountModel()): SutTypes => {
+    const history = createMemoryHistory({ initialEntries: ['/'] })
+    render(
+        <ApiContext.Provider value={{ getCurrentAccount: () => account }} >
             <Router history={history}>
                 <PrivateRoute />
             </Router>
-        )
+        </ApiContext.Provider>
+    )
+    return { history }
+}
+
+describe('PrivateRoute', () => {
+    test('Rediricionar para logim se nao tiver o token de acesso', () => {
+        const { history } = makeSut(null)
         expect(history.location.pathname).toBe('/login')
+    })
+
+    test('deve renderizar o componente atual se o token não estiver vazio', () => {
+        const { history } = makeSut()
+        expect(history.location.pathname).toBe('/')
     })
 })
