5056080f45e8f5b3bee9efbb6f9dda25de96afdc
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-25 10:21:08 -0300

    feat: criando o makeSut

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import { render, screen} from '@testing-library/react'
 import { SurveyList } from '@/presentation/pages'
 
+const makesut = (): void => {
+    render(<SurveyList />)
+}
+
 describe('SurveyList Componente', () => {
   test('testando o estado inicial da pagina com 4 itens', () => {
-    render(<SurveyList />)
+    makesut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
   })
