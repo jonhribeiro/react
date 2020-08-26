c997f7163516d46cb962ef49af2bb0d20146c195
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-26 15:37:21 -0300

    feat: deve chamar axios.get com o URL correto com valores

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -53,7 +53,7 @@ describe('AxiosHttpClient', () => {
             const request = mockGetRequest()
             const {sut, mockedAxios} = makeSut()
             await sut.get(request)
-            expect(mockedAxios.get).toHaveBeenCalledWith(request.url)
+            expect(mockedAxios.get).toHaveBeenCalledWith(request.url, { headers: request.headers })
         }) 
         
         test('deve retornar o código de status corretos com axios.get', async () => {
