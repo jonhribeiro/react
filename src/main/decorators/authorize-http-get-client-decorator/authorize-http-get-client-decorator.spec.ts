32fc60a03777cf67a5a1dfb0a2439e5db720d086
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-26 16:57:15 -0300

    feat: criando o makeSut

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,10 +1,23 @@
 import { AuthorizeHttpGetClientDecorator } from '@/main/decorators'
 import { GetStorageSpy, mockGetRequest } from '@/data/test'
 
-describe('AuthorizeHttpGetClientDecorator', () => {
-  test('deve chamar GetStorage com o valor correto', () => {
+type SutTypes = {
+    sut: AuthorizeHttpGetClientDecorator
+    getStorageSpy: GetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
     const getStorageSpy = new GetStorageSpy()
     const sut = new AuthorizeHttpGetClientDecorator(getStorageSpy)
+    return {
+        sut,
+        getStorageSpy
+    }
+}
+
+describe('AuthorizeHttpGetClientDecorator', () => {
+  test('deve chamar GetStorage com o valor correto', () => {
+    const { sut, getStorageSpy } = makeSut()
     sut.get(mockGetRequest())
     expect(getStorageSpy.key).toBe('account')
   })
