265886889e1495dc587591a0f03f08364cceddd2
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-28 15:09:57 -0300

    refactor: validando os campos nos teste

--- src/main/factories/pages/login/login-validation-factory.spec.ts
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -1,13 +1,14 @@
-import { makeLoginValidation } from "./login-validation-factory"
-import { ValidationComposite } from "@/validation/validators"
-import { ValidationBuilder } from "@/validation/validators/builder/validation-buiulder"
+import { makeLoginValidation } from '@/main/factories/pages'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation, MinLengthValidation } from "@/validation/validators"
 
 describe('LoginValidationFactory', () => {
     test('deve makeValidationComposite com validações corretas', () => {
         const composite = makeLoginValidation()
         expect(composite).toEqual(ValidationComposite.build([
-            ...ValidationBuilder.field('email').required().email().build(),
-            ...ValidationBuilder.field('password').required().min(5).build()
+            new RequiredFieldValidation('email'),
+            new EmailValidation('email'),
+            new RequiredFieldValidation('password'),
+            new MinLengthValidation('password', 5)
         ]))
 
     })
--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,15 +1,18 @@
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-buiulder'
-import { makeSignUpValidation } from './signup-validation-factory'
+import { CompareFieldsValidation, ValidationComposite, RequiredFieldValidation, MinLengthValidation, EmailValidation } from '@/validation/validators'
+import { makeSignUpValidation } from '@/main/factories/pages'
 
 describe('SignUpValidationFactory', () => {
     test('deve makeValidationComposite com validações corretas', () => {
         const composite = makeSignUpValidation()
         expect(composite).toEqual(ValidationComposite.build([
-            ...Builder.field('name').required().min(5).build(),
-            ...Builder.field('email').required().email().build(),
-            ...Builder.field('password').required().min(5).build(),
-            ...Builder.field('passwordConfirmation').required().sameAs('password').build()
+            new RequiredFieldValidation('name'),
+            new MinLengthValidation('name', 5),
+            new RequiredFieldValidation('email'),
+            new EmailValidation('email'),
+            new RequiredFieldValidation('password'),
+            new MinLengthValidation('password', 5),
+            new RequiredFieldValidation('passwordConfirmation'),
+            new CompareFieldsValidation('passwordConfirmation', 'password')
         ]))
 
     })
