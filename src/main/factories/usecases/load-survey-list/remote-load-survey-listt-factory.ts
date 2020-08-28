39b0a57fb337dacba6d11a26141e07f1eda50c01
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-08-28 09:00:15 -0300

    feat: aplicando o decorator no tela

--- src/main/factories/usecases/load-survey-list/remote-load-survey-listt-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-listt-factory.ts
@@ -1,8 +1,9 @@
-import { makeApiUrl, makeAxiosHttpClient } from '@/main/factories/http'
+import { makeApiUrl } from '@/main/factories/http'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
 import { LoadSurveyList } from '@/domain/usercases'
 import { RemoteLoadSurveyList } from '@/data/usecases'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-    return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+    return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAuthorizeHttpGetClientDecorator())
 
 }
