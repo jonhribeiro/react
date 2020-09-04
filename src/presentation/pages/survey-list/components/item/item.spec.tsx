eb510c3ded18e80a92d1e9c5531b6b5593ea5649
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-09-04 10:49:12 -0300

    refactor: corrigindo o teste de data

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -11,19 +11,27 @@ const makeSut = (survey = mockSurveyModel()): void => {
 describe('SurveyItem Componente', () => {
   test('renderizar o componete com valores correto', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true
+      didAnswer: true,
+      date: new Date('2020-08-25T00:00:00')
     })
     makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('25')
+    expect(screen.getByTestId('month')).toHaveTextContent('ago')
+    expect(screen.getByTestId('year')).toHaveTextContent('2020')
   })
 
   test('renderizar o componete com valores correto', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: false
+      didAnswer: false,
+      date: new Date('2019-05-03T00:00:00')
     })
     makeSut(survey)
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('mai')
+    expect(screen.getByTestId('year')).toHaveTextContent('2019')
   })
 })
