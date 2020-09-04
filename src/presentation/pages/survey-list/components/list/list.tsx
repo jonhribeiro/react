141e7213bf7aedc0c55f07af3f70b077f86da5b8
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-09-04 10:36:24 -0300

    refactor: removendo o context

--- src/presentation/pages/survey-list/components/list/list.tsx
+++ src/presentation/pages/survey-list/components/list/list.tsx
@@ -1,14 +1,17 @@
-import { SurveyItem, SurveyItemEmpty, SurveyContext } from '@/presentation/pages/survey-list/components'
+import { SurveyItem, SurveyItemEmpty } from '@/presentation/pages/survey-list/components'
 import Styles from './list-styles.scss'
-import React, { useContext } from 'react'
+import React from 'react'
 import { LoadSurveyList } from '@/domain/usercases'
 
-const List: React.FC = () => {
-  const { state } = useContext(SurveyContext)
+type Props = {
+  surveys: LoadSurveyList.Model[]
+}
+
+const List: React.FC<Props> = ({ surveys }: Props) => {
   return (
     <ul className={Styles.listWrap} data-testid="survey-list">
-      {state.surveys.length
-        ? state.surveys.map((survey: LoadSurveyList.Model) => <SurveyItem key={survey.id} survey={survey} />)
+      {surveys.length
+        ? surveys.map((survey: LoadSurveyList.Model) => <SurveyItem key={survey.id} survey={survey} />)
         : <SurveyItemEmpty />
       }
     </ul>
