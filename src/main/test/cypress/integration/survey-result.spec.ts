175fe67c0759117a6dafe85740d3fab38ca21505
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-09-08 09:17:02 -0300

    test: deve apresentar erro em UnecpectedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -3,8 +3,6 @@ import * as Http from '../utils/http-mocks'
 
 const path = /surveys/
 const mockUnexpectedError = (): void => Http.mockServerError(path, 'GET')
-// const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
-// const mockSuccess = (): void => Http.mockOk(path, 'GET', 'fix:survey-list')
 
 describe('SurveyResult', () => {
   beforeEach(() => {
@@ -18,30 +16,4 @@ describe('SurveyResult', () => {
     cy.visit('/surveys/any_id')
     cy.getByTestId('error').should('contain.text', 'Algo errado aconteceu. tente novamente mais tarde')
   })
-
-  // it('deve fazer logout AccessDeniedError', () => {
-  //   mockAccessDeniedError()
-  //   cy.visit('')
-  //   Helper.testUrl('/login')
-  // })
-
-  // it('deve ter o username na barra', () => {
-  //   mockUnexpectedError()
-  //   cy.visit('')
-  //   const { name } = Helper.getLocalStorageItem('account')
-  //   cy.getByTestId('username').should('contain.text', name)
-  // })
-
-  // it('deve fazer logout ao clicar no botao', () => {
-  //   mockUnexpectedError()
-  //   cy.visit('')
-  //   cy.getByTestId('logout').click()
-  //   Helper.testUrl('/login')
-  // })
-
-  // it('deve apresentar itens de pesquisa', () => {
-  //   mockSuccess()
-  //   cy.visit('')
-  //   cy.get('li:empty').should('have.length', 4)
-  // })
 })
