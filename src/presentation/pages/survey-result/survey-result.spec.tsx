685541915bf5f7f87aad86088b4508ea5c339182
Jonh Ribeiro de Araujo <jonh.araujo@aparecida.go.gov.br>
2020-09-09 09:07:10 -0300

    test: deve renderizar o erro em caso de falha em save

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -141,4 +141,18 @@ describe('SurveyResult Component', () => {
     })
     await waitFor(() => screen.getByTestId('survey-result'))
   })
+
+  test('deve renderizar o erro em caso de falha em save', async () => {
+    const saveSurveyResultSpy = new SaveSurveyResultSpy()
+    const error = new UnexpectedError()
+    jest.spyOn(saveSurveyResultSpy, 'save').mockRejectedValueOnce(error)
+    makeSut({ saveSurveyResultSpy })
+    await waitFor(() => screen.getByTestId('survey-result'))
+    const answersWrap = screen.queryAllByTestId('answer-wrap')
+    fireEvent.click(answersWrap[1])
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(screen.queryByTestId('question')).not.toBeInTheDocument()
+    expect(screen.getByTestId('error')).toHaveTextContent(error.message)
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
 })
